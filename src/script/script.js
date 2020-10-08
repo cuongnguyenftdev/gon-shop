b6f5a29cb3beeec9b0bf8362819faa6fdb9d4301
cuongnv9899 <50770206+cuongnv9899@users.noreply.github.com>
2020-10-09 02:27:18 +0700

    #

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,4 @@
 
-// import swal from 'sweetalert';
-
 /* ----- reload website  ----- */
 document.querySelector('header > .mid-header > img').addEventListener('click', () => {
     location.reload();
@@ -11,10 +9,12 @@ document.querySelector('header > .bottom-header > ul li:first-child').addEventLi
 
 /* ----- scroll to top or bottom of the website ----- */
 
+// scroll to top
 var scrollBtn = document.querySelectorAll('.scroll i');
 scrollBtn[0].addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 })
+// scroll to bottom
 scrollBtn[1].addEventListener('click', () => {
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
 })
@@ -33,14 +33,14 @@ search.addEventListener('click', function () {
 var category = document.querySelector('.mid-header .categories');
 category.addEventListener('click', () => {
     var ul = document.querySelector('.mid-header .categories ul');
-    ul.classList.toggle('is-show');
+    ul.classList.toggle('is-show'); // show category list
 });
 
 var ulCategory = document.querySelector('.mid-header .categories ul');
 ulCategory.addEventListener('click', (event) => {
     var content = event.target.innerHTML;
-    document.querySelector('.mid-header .categories span').innerHTML = content;
-    category.classList.toggle('is-show');
+    document.querySelector('.mid-header .categories span').innerHTML = content; // show the user's choice
+    category.classList.toggle('is-show'); // close category list
 })
 
 /* -----  zoom the image when click to it ----- */
@@ -49,20 +49,24 @@ var getImg = document.querySelector('.products .product');
 getImg.addEventListener('click', (event) => {
     var url = event.target.src;
     document.querySelector('.products .zoom img').src = url;
+    // set the target div is current
+    document.querySelector('.products .product div.current').classList.remove('current');
+    event.target.parentElement.classList.toggle('current');
 });
+
 
 
 /* ----- feature add-to-cart ----- */
 
-var amountBtn = document.querySelectorAll('.buy .amount button');
-var amount = document.querySelector('.buy .amount span');
+var amountBtn = document.querySelectorAll('.buy .amount button'); // get two button increase and decrease
+var amount = document.querySelector('.buy .amount span');   // the amount that the user choice
 
 // amountBtn[0] is increase
 amountBtn[0].addEventListener('click', () => {
-    if (parseInt(amount.innerHTML) >= 5) {
+    if (parseInt(amount.innerHTML) >= 5) { // if the user chooses more than the remaining amount, a notification will be sent
         swal({
             text: 'Our shop does not have the quantity you need. Please feel free to contact us. Thank you!',
-            icon: "warning",
+            icon: "info",
         });
         return;
     }
@@ -71,7 +75,45 @@ amountBtn[0].addEventListener('click', () => {
 
 // amountBtn[1] is decrease
 amountBtn[1].addEventListener('click', () => {
-    if (amount.innerHTML != 0) {
+    if (amount.innerHTML > 1) {     // only active when amount is greater than 1
         amount.innerHTML = (amount.innerHTML - 1);
     }
+});
+
+// add-to-cart button
+
+var addToCartBtn = document.querySelector('.buy .buy-btn'); // add to cart button
+
+addToCartBtn.addEventListener('click', () => {
+    //check out of stock
+    if (document.querySelector('.product .current > div').classList.contains('out-of-stock')) {
+        swal({
+            text: "Sorry. Out of stock :(",
+        })
+        return;
+    }
+    // set value of the cart
+    var value = document.querySelector('.price span').innerHTML.split('$');
+    var currentValue = document.querySelector('.cart span').innerHTML.split("$");
+    document.querySelector('.cart span').innerHTML = "Cart: $" + (parseFloat(currentValue[1]) + parseFloat(amount.innerHTML) * parseFloat(value[1])).toFixed(2);
+
+    // add out-of-stock tag to product if user bought 5 products
+    if (parseInt(amount.innerHTML) == 5) {
+        document.querySelector('.product .current > div').classList.add('out-of-stock');
+    }
+    // amount reset
+    amount.innerHTML = "1";
+
+    //message
+    swal({
+        text: "Add to cart successfully",
+        icon: "success",
+    });
+
+});
+
+// click to "love button"
+
+document.querySelector('.love').addEventListener('click', () => {
+    document.querySelector('.love').classList.toggle("isLove");
 });
